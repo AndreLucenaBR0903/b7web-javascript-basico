59ce057eaa1d9336a74018d1578e3f728e213c4b
Andre Lucena <tiozaoinveste.br@gmail.com>
2025-09-27 09:12:02 -0300

    Adicionando mais exercícios

---
+++ exercicio05.js
@@ -0,0 +1,10 @@
+let fruits = ["Banana", "Apple", "Orange"];
+for (let x in fruits) {
+  console.log(fruits[x]);
+}
+
+let number = 1;
+while (number <= 100) {
+  console.log(number);
+  number++;
+}
